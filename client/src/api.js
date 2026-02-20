fc2847555c6cdc9fc01bdadf46f01f7ace7280d0
Zyif <fiyazahmd002@gmail.com>
2026-02-21 02:18:39 +0600

    fix

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    "https://coffee-shop-mern.onrender.com",
 });
 
 export default API;
