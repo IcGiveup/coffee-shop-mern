35c25814f5b4dc6f0c93f1fadccf5e056547de15
Zyif <fiyazahmd002@gmail.com>
2026-02-21 00:48:39 +0600

    modal import

--- routes/adminOrderRoutes.js
+++ routes/adminOrderRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Order = require("../models/OrderModel");
+const Order = require("../models/orderModel");
 
 router.get("/", async (req, res) => {
   try {
