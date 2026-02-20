12c92f710fa8f0317a0263af66bc07cc6d41674a
Zyif <fiyazahmd002@gmail.com>
2026-02-21 02:12:04 +0600

    Fix reducer

--- client/src/store.js
+++ client/src/store.js
@@ -2,13 +2,16 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+
 import { cartReducer } from './reducers/cartReducer';
 import { loginUserReducer, registerUserReducer } from './reducers/userReducer';
+import coffeeReducer from './reducers/coffeeReducer'; // ✅ ADD THIS
 
 const reducer = combineReducers({
   cart: cartReducer,
   loginUser: loginUserReducer,
   registerUser: registerUserReducer,
+  coffee: coffeeReducer, // ✅ ADD THIS
 });
 
 const middleware = [thunk];
